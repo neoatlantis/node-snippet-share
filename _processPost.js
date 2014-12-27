132f5780b3db46dfec2385be005060590338487b
neoatlantis <rijndael@yeah.net>
2014-12-27 19:24:58 +0100

    save

--- _processPost.js
+++ _processPost.js
@@ -24,5 +24,43 @@ module.exports = function(params, response){
     };
 
     // save
+    var mongoClient = require('mongodb').MongoClient;
+    mongoClient.connect(params.config.server.mongodb, function(err, db){
+        if(err){
+            response.writeHead(500);
+            response.end();
+            return console.error('Error saving a snippet during connection');
+        };
 
+        var collection = db.collection('snippets');
+        collection.insert({
+            validTo: new Date().getTime() + params.config.snippet.life * 1000,
+            base64: data.encode('base64'),
+        }, function(err, result){
+            if(err){
+                response.writeHead(500);
+                response.end();
+                return console.error('Error saving a snippet during saving.');
+            };
+
+            try{
+                var last = db.inventory.find();
+                var id = last._id;
+            } catch(e){
+                response.writeHead(500);
+                console.error(e);
+                response.end();
+                db.close();
+                return;
+            };
+
+
+            console.log("New " + String(result.result.n) + " snippet.");
+            db.close();
+
+            response.writeHead(200);
+            response.write(String(id));
+            response.end();
+        });
+    });
 };
